ca8398b987cf237cd9ee11fbedc9a8955f89c2c4
JoshuaT <jttsat170@gmail.com>
2024-12-28 17:38:57 -0500

    Testing out functions to see if they work

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 import inquirer from 'inquirer';
 import { writeFile } from 'fs';
 
-inquirer
-.prompt([
-{
-    type: 'input',
-    name: 'name',
-    message: 'What is your name'
+const questions = [
+    "What is you name",
+    "Project Title",
+    "Enter description",
+    "Installation instructions",
+    "Usage information",
+    "Test Instructions"
+]
+
+const promptUser = () =>{
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name'
+    }
+    ])
 }
-])
-.then((data)=>{
-  writeFile("index.html", data.name, (err)=>{
-    if (err) throw err;
-    console.log("The file has been created")
-  })
-})
+
+const init = () =>{
+    promptUser()
+    .then((data)=>{
+        writeFile("index.html", data.name, (err)=>{
+          if (err) throw err;
+          console.log("The file has been created")
+        })
+      })
+}
+
+init()
