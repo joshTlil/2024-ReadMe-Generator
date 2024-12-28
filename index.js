1c231c2ddaf8c976dede81d16acfe886072b4a2b
JoshuaT <jttsat170@gmail.com>
2024-12-28 17:28:19 -0500

    Working with writefile and inquirer packages

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log("Hello")
+import inquirer from 'inquirer';
+import { writeFile } from 'fs';
+
+inquirer
+.prompt([
+{
+    type: 'input',
+    name: 'name',
+    message: 'What is your name'
+}
+])
+.then((data)=>{
+  writeFile("index.html", data.name, (err)=>{
+    if (err) throw err;
+    console.log("The file has been created")
+  })
+})
